0470563aac07105470eb2b1f2e73d64f321a5a0d
robert <robertrenbysanjuan@gmail.com>
2025-08-31 10:21:59 +0800

    feat: add Header component with navbar, logo, navigation links, and properties dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
+import Layout from "./components/layoutComponents/Layout"
 
 function App() {
 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      
+    </Route>
+  ))
+
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/layoutComponents/Header.jsx
+++ src/components/layoutComponents/Header.jsx
@@ -1,7 +1,28 @@
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-[rgba(213,116,13,0.41)]">
+        <nav className="flex justify-between items-center py-2 px-10">
+            <img src="/logo.png" alt="logo" className="w-[75px] h-[60px] cursor-pointer"/>
+            
+            <ul className="flex  gap-3 ">
+                <li><NavLink to='/home'>Home</NavLink></li>
+                <li>
+                    <select name="properties">
+                        <option hidden>Properties</option>
+                        <option value="high-end">HIGH END</option>
+                        <option value="townhomes">TOWNHOMES</option>
+                        <option value="farmlots">FARM LOTS</option>
+                    </select>
+                </li>
+                <li><NavLink to='/realtors'>Realtors</NavLink></li>
+                <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/FAQs'>FAQs</NavLink></li>
+                <li><NavLink to='/contact'>Contact</NavLink></li>
+            </ul>
+        </nav>
+    </header>
   )
 }
 
